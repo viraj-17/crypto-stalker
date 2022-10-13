ce2e9f9d1aea93859929fa216c87fb6ebcc35b77
viraj-17 <virajramdin01@gmail.com>
2022-10-13 08:24:26 +0530

    First Commit

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Banner from "../components/Banner/Banner";
+import CoinsTable from "../components/CoinsTable";
+
+const Homepage = () => {
+  return (
+    <>
+      <Banner />
+      <CoinsTable />
+      <footer>
+        <p>created with ❤️ by viraj</p>
+      </footer>
+    </>
+  );
+};
+
+export default Homepage;
